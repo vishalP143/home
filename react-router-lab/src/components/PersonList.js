68c74e632ce91e68d6d3bc82e7a543fc06cee332
vishalP143 <vishal.p8070@gmail.com>
2024-10-04 06:13:59 +0000

    today

--- react-router-lab/src/components/PersonList.js
+++ react-router-lab/src/components/PersonList.js
@@ -2,16 +2,16 @@ import React from 'react';
 import axios from 'axios';
 import '../styles/PersonList.css'
 
-// const API_URL= process.env.REACT_APP_API_URL
-const output = axios.get("https://5000-vishalp143-home-nxvii7uvu5o.ws-us116.gitpod.io/persons")
+const API_URL= process.env.REACT_APP_API_URL
+const output = axios.get("https://5000-vishalp143-home-3ea2ftkv3tu.ws-us116.gitpod.io/persons")
 const PersonList = () => {
 
 
     console.log(output);
-    // console.log(API_URL)
+    console.log(API_URL)
     
   return (
-    <div>
+    <div class="person-list">
       <h2>This is the Person List</h2>
     </div>
   );
